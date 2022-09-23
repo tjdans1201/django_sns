1934230e1019efc6f7caa96d3f9082294543056f
Seongmun <tjdans1201@gmail.com>
2022-09-23 13:53:09 +0900

    Feat: Board 모델 추가

--- apps/boards/models.py
+++ apps/boards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from ..users.models import User
+
+
+class Board(models.Model):
+    index = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    hashtag = models.CharField(max_length=50)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    heart_count = models.IntegerField()
+    views_count = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
 
 # Create your models here.
